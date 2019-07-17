68c22916ff36a1330fa0c66767affa479adbabb7
Benjamin Truong <bentruhoa@gmail.com>
2019-07-17 02:55:43 -0500

    Add files via upload

--- tipCal.js
+++ tipCal.js
@@ -1,3 +1,42 @@
+// function calTip(){
+// var billTotal = document.getElementByID("billAmt").value;
+// console.log(billTotal);
+
+// var Service = document.getElementByID("serviceAmt").value;
+// console.log(billTotal);
+
+// //Checks to see if billAmount is a number
+// if (billTotal.isInteger()== false)
+// {
+//   alert("Please enter the bill amount");
+//   return;
+// }
+
+// // Checks to see if customer chose an option
+// if(Service == -1)
+//   {
+//     alert("Please choose an option");
+//     return;
+//   }
+
+// var TipAmt= billTotal*Service;
+//   document.getElementbyID("tipDisplay").style.display="block";
+  
+//   document.getElementbyID("tip").innerHTML=TipAmt;
+  
+// }
+
+// //hide elements on load
+// document.getElementbyID("tipDisplay").style.display="none";
+
+// //how to activie on click of button
+// document.getElementbyID("Cal").onclick=function()
+// {
+//   calTip();
+// };
+
+
+
 //hide elements on load
 /*Moved this to top for clarity */
 document.getElementById('tipDisplay').style.display = 'none';
@@ -8,13 +47,21 @@ function calTip()
   let billTotal = document.getElementById('billAmt').value;
   /* Check out the type that you are getting back when you parse the value from the DOM */
   console.log(typeof billTotal);
+  console.log('billTotal: ' + billTotal);
+  let billConvert = parseFloat(billTotal);
+  console.log(typeof billConvert);
+  console.log('billConvert:'+ billConvert);
 
   /* Changing variable declaration to es6 */
   let Service = document.getElementById('serviceAmt').value;
-  console.log('billTotal: ' + billTotal);
-
+  console.log(typeof Service);
+  let serviceConvert = parseFloat(Service);
+  console.log(typeof serviceConvert);
+  
+  let TipAmt = billConvert * serviceConvert;
+  console.log(TipAmt);
   //Checks to see if billAmount is a number
-  if (Number.isInteger(billTotal) === false)
+  if (billConvert <= 0)
   {
     alert("Please enter the bill amount");
     return;
@@ -27,11 +74,15 @@ function calTip()
       return;
     }
 
-  var TipAmt = billTotal * Service;
-  document.getElementByID("tipDisplay").style.display="block";
   
-  document.getElementByID("tip").innerHTML = TipAmt;
+  
+  document.getElementById("tipDisplay").style.display="block";
+  
+  document.getElementById("tip").innerHTML = TipAmt.toFixed(2);
 }
 
 // passing the function to the onclick listener
 document.getElementById('Cal').onclick = calTip;
+
+
+
